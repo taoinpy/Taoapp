845f7a5a2c831f10806613c7a31525863cb5e2dd
tao <xaybundith1168@gmail.com>
2016-12-13 16:15:32 +0700

    Initial commit

---
+++ app/src/main/java/ltc/tao/taoapp/MainActivity.java
@@ -0,0 +1,20 @@
+package ltc.tao.taoapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void taoClick(View view) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+    }
+}
